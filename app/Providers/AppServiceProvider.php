44b2c058a1dd97fd7ac10b20262c842dda7af2de
Mr.WOLF-WS <gaiththewolf@gmail.com>
2026-05-08 16:56:02 +0100

    fix logviewer authorization for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('downloads', function (Request $request): Limit {
             return Limit::perMinute(30)->by($request->user()?->id ?: $request->ip());
         });
+
+        if (app()->environment('production')) {
+            LogViewer::auth(function ($request) {
+                $user = $request->user();
+                return $user && $user->hasRole('admin');
+            });
+        }
     }
 }
